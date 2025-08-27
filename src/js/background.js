37beed435571a453e935cb95f568238b12a9ba83
KKamaa <kevkamaa96@gmail.com>
2025-08-27 06:05:09 +0300

    [FIX] seticon on chrome manifest v3

--- src/js/background.js
+++ src/js/background.js
@@ -1,32 +1,36 @@
-/*
-    Copyright 2016 - 2022 Therp (https://therp.nl)
-    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
- */
+// ---- one API alias for Chrome/Firefox ----
+const api = (typeof chrome !== 'undefined' && chrome.runtime)
+  ? chrome                          // Chrome (and Chromium)
+  : (typeof browser !== 'undefined' ? browser : null); // Firefox
 
-var online = "it is online";
-console.log(online);
-var TogglButton = {
-    setBrowserAction: function (timer) {
-        console.log("TIMER CHECK:" + timer);
-        if (timer === true) {
-            var imagePath = {'19': 'img/icon_19.png', '38': 'img/icon_38.png'};
-        }
-        else if (timer === 'pause') {
-            var imagePath = {'19': 'img/icon-pause.png', '38': 'img/icon-pause.png'};
-        }
-        else {
-            var imagePath = {'19': 'img/inactive_19.png', '38': 'img/inactive_19.png'};
-        }
-        browser.action.setIcon({
-	      path: imagePath,
-	    });
-	    console.log("works");
-    },
+if (!api) throw new Error('No extension API found');
+
+// helpers
+const R = api.runtime;
+const A = api.action || api.browserAction;  // MV3 uses action
+const S = api.storage;
+const url = (p) => R.getURL(p);             // build extension-absolute URLs
+
+const TogglButton = {
+  setBrowserAction(timer) {
+    console.log('TIMER CHECK:', timer);
+    let path;
+    if (timer === true || String(timer).toLowerCase() === 'true') {
+      path = { 19: url('img/icon_19.png'), 38: url('img/icon_38.png') };
+    } else if (String(timer).toLowerCase() === 'pause') {
+      path = { 19: url('img/icon-pause.png'), 38: url('img/icon-pause.png') };
+    } else {
+      path = { 19: url('img/inactive_19.png'), 38: url('img/inactive_38.png') };
+    }
+    const p = A.setIcon({ path });
+    if (p?.catch) p.catch(err => console.error('setIcon failed:', err));
+  },
 };
-browser.runtime.onMessage.addListener(async (msg, sender) => {
-  console.log("BG page received message", msg, "from", sender);
-  console.log("Stored data", await browser.storage.local.get());
-  TogglButton.setBrowserAction(msg.TimerActive);
+
+// Message listener (works in both)
+api.runtime.onMessage.addListener(async (msg, sender) => {
+  console.log('BG got message', msg, 'from', sender);
+  const data = await S?.local?.get?.(['TimerActive']);
+  console.log('Stored data', data);
+  TogglButton.setBrowserAction(msg?.TimerActive);
 });
-
-
